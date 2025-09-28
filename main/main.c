1f5111629cf5b12646547d6cc616ef03fed181e1
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-09-29 01:17:43 +0300

    call system_state_init() in main.c

--- main/main.c
+++ main/main.c
@@ -1,19 +1,37 @@
+// Copyright (C) 2025 Stratos Thivaios
+//
+// This file is part of "EDLAVP-ESP-FW".
+//
+// "EDLAVP-ESP-FW" is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// "EDLAVP-ESP-FW" is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// "EDLAVP-ESP-FW". If not, see <https://www.gnu.org/licenses/>.
+
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "nvs_flash.h"
+#include "system_state.h"
 #include "wifi_manager.h"
 #include <stdio.h>
 
 static const char *TAG = "MAIN";
 
-void app_main(void)
-{
-  ESP_LOGI(TAG, "Starting WiFi");
-
-  // Initialize NVS (required)
+void app_main(void) {
+  // initialize NVS
   nvs_flash_init();
 
-  // Connect to WiFi
+  // initialize event group bits
+  system_state_init();
+
+  // attempt to start wifi stuff
   wifi_connect();
 }
