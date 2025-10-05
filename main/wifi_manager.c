52fe42885f1ba295647807c613dd01f0b847c798
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-10-06 00:33:40 +0300

    ESP_ERROR_CHECK in critical places in wifi_manger.c

--- main/wifi_manager.c
+++ main/wifi_manager.c
@@ -156,19 +156,19 @@ void wifi_connect(void) {
   ESP_LOGI(TAG, "Setting up WiFi...");
 
   // initialize networking
-  esp_netif_init();
+  ESP_ERROR_CHECK(esp_netif_init());
   esp_event_loop_create_default();
   esp_netif_create_default_wifi_sta();
 
   // initialize wifi
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  esp_wifi_init(&cfg);
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
   // register event handlers
-  esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler,
-                             NULL);
-  esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler,
-                             NULL);
+  ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
+                                             &wifi_event_handler, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
+                                             &ip_event_handler, NULL));
 
   // define the wifi config
   wifi_config_t wifi_config = {
@@ -180,7 +180,7 @@ void wifi_connect(void) {
   };
 
   // configure wifi and start
-  esp_wifi_set_mode(WIFI_MODE_STA);
-  esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
-  esp_wifi_start();
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
 }
