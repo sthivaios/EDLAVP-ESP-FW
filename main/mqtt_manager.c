82a1db0042b671e8c1975aefd81ed5a96680c4ca
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-10-10 01:30:17 +0300

    oops

--- main/mqtt_manager.c
+++ main/mqtt_manager.c
@@ -48,8 +48,8 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
     break;
 
   case MQTT_EVENT_PUBLISHED:
-    // ESP_LOGI(TAG, "Published a message to an MQTT topic: msg_id=%d",
-    //          event->msg_id);
+    ESP_LOGI(TAG, "Published a message to an MQTT topic: msg_id=%d",
+             event->msg_id);
     break;
 
   case MQTT_EVENT_DATA:
