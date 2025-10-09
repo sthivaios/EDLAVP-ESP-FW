3d3a0a40c755aa76042cbc4810914960b2427abc
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-10-10 02:03:49 +0300

    license header missing from some files

--- main/mqtt_manager.c
+++ main/mqtt_manager.c
@@ -1,3 +1,12 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (C) 2025 Stratos Thivaios
+//
+// EDLAVP-ESP-FW - The ESP-IDF Version of the EDLAVP firmware
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #include "mqtt_manager.h"
 
 #include "cJSON.h"
--- main/sensor_manager.h
+++ main/sensor_manager.h
@@ -1,8 +1,18 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (C) 2025 Stratos Thivaios
+//
+// EDLAVP-ESP-FW - The ESP-IDF Version of the EDLAVP firmware
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #ifndef _SENSOR_MANAGER_H
 #define _SENSOR_MANAGER_H
 
 #define SENSOR_MANAGER_TASK_STACK_SIZE 8192
-#define ONEWIRE_MAX_RX_BYTES 10 // 1byte ROM command + 8byte ROM number + 1byte device command
+#define ONEWIRE_MAX_RX_BYTES                                                   \
+  10 // 1byte ROM command + 8byte ROM number + 1byte device command
 
 void sensor_manager(void *pvParameters);
 
