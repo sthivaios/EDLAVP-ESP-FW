72a9b0a29c5fa81ac46dc36c74938d92afb29556
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-10-22 01:29:40 +0300

    use the UniversalSingleReadout type in system_state.c queue functions

--- main/system_state.c
+++ main/system_state.c
@@ -11,6 +11,7 @@
 #include "esp_log.h"
 #include "freertos/event_groups.h"
 #include "sensor_manager.h"
+#include "types.h"
 
 static const char *TAG = "SYSTEM_STATE";
 static EventGroupHandle_t s_event_group = NULL;
@@ -25,7 +26,7 @@ static QueueHandle_t readout_queue;
  */
 void readout_queue_init(void) {
   readout_queue = xQueueCreate(CONFIG_SOFTWARE_DS18B20_READOUT_QUEUE_SIZE,
-                               sizeof(DS18B20SingleReadout));
+                               sizeof(UniversalSingleReadout));
   if (readout_queue == NULL) {
     ESP_LOGE(TAG, "FATAL: Queue creation failed!");
     abort();
@@ -42,7 +43,7 @@ void readout_queue_init(void) {
  * @param ticks_to_wait Maximum number of ticks to wait if the queue is full.
  * @return pdPASS if the value was successfully enqueued, pdFAIL otherwise.
  */
-BaseType_t readout_queue_send(const DS18B20SingleReadout readout,
+BaseType_t readout_queue_send(const UniversalSingleReadout readout,
                               const TickType_t ticks_to_wait) {
   if (readout_queue == NULL)
     return pdFAIL;
@@ -60,7 +61,7 @@ BaseType_t readout_queue_send(const DS18B20SingleReadout readout,
  * @param ticks_to_wait Maximum number of ticks to wait if the queue is empty.
  * @return pdPASS if a value was successfully received, pdFAIL otherwise.
  */
-BaseType_t readout_queue_receive(DS18B20SingleReadout *readout,
+BaseType_t readout_queue_receive(UniversalSingleReadout *readout,
                                  const TickType_t ticks_to_wait) {
   if (readout_queue == NULL || readout == NULL)
     return pdFAIL;
