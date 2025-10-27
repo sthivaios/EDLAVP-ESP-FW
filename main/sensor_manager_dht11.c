eb5bf9dc0d0fbeda3239ca33dabe4b91cfcdddd2
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-10-27 02:13:42 +0200

    fetch the humidity in sensor_manager_dht11.c

--- main/sensor_manager_dht11.c
+++ main/sensor_manager_dht11.c
@@ -9,6 +9,8 @@
 
 #include "sensor_manager_dht11.h"
 
+#include "dht.h"
+#include "driver/gpio.h"
 #include "esp_err.h"
 #include "esp_log.h"
 #include "system_state.h"
@@ -20,15 +22,26 @@ static const char *TAG = "sensor_manager_dht11";
 void sensor_manager_dht11(void *pvParameters) {
   ESP_LOGI(TAG, "%s task started", TAG);
 
-  // get sensor
-
   while (1) {
-    system_wait_for_bits(SYS_BIT_SENSOR_READ_REQUESTED, pdTRUE, portMAX_DELAY);
+    system_wait_for_bits(SYS_BIT_DHT11_READ_REQUESTED, pdTRUE, portMAX_DELAY);
     system_wait_for_bits(SYS_BIT_NTP_SYNCED, pdTRUE, portMAX_DELAY);
 
     // get readout
+    float temperature, humidity;
+    time_t now;
 
-    const UniversalSingleReadout readout = {.value = temperature,
+#ifdef CONFIG_HARDWARE_DHT11_INTERNAL_PULLUP
+    gpio_set_pull_mode(CONFIG_HARDWARE_DHT11_GPIO_PIN, GPIO_PULLUP_ONLY);
+#endif
+
+    if (dht_read_float_data(DHT_TYPE_DHT11, CONFIG_HARDWARE_DHT11_GPIO_PIN,
+                            &humidity, &temperature) != ESP_OK) {
+      ESP_LOGW(TAG, "Could not read data from the DHT11 sensor!");
+      continue;
+    }
+    time(&now);
+
+    const UniversalSingleReadout readout = {.value = humidity,
                                             .timestamp = now,
                                             .sensor_type = "dht11",
                                             .unit = "%"};
@@ -36,9 +49,9 @@ void sensor_manager_dht11(void *pvParameters) {
     if (readout_queue_send(readout, pdMS_TO_TICKS(100)) != pdPASS) {
       ESP_LOGW(TAG, "Queue full, dropping readout!");
     } else {
-      ESP_LOGI(TAG, "READOUT QUEUED -> DS18B20: %.2f", temperature);
+      ESP_LOGI(TAG, "READOUT QUEUED -> DHT11: %.2f (humidity)", humidity);
     }
 
-    system_clear_bits(SYS_BIT_SENSOR_READ_REQUESTED);
+    system_clear_bits(SYS_BIT_DHT11_READ_REQUESTED);
   }
 }
--- main/sensor_manager_dht11.h
+++ main/sensor_manager_dht11.h
@@ -7,9 +7,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#ifndef _SENSOR_MANAGER_H
-#define _SENSOR_MANAGER_H
+#ifndef _SENSOR_MANAGER_DHT11_H
+#define _SENSOR_MANAGER_DHT11_H
 
 void sensor_manager_dht11(void *pvParameters);
 
-#endif //_SENSOR_MANAGER_H
+#endif //_SENSOR_MANAGER_DHT11_H
